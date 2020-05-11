1b3e5c514f83ac2d28c176f37a5c05da3ae294e1
OlegSadJktu <plyuha2004@mail.ru>
2020-05-11 11:37:24 +0600

    починил менюшку

--- js/js.js
+++ js/js.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-	$('.menu-button').click(function(){ $('#mySidenav').css('width','14vw'); $("body").css('marginLeft','14vw'); });
+openMenu()
 
+function openMenu(){
+	let counter = 1;
 
-			})
+	$('.menu-button').click(function(){
+		if (counter % 2 == 1) {
+
+			$('#mySidenav').css('width','14vw');
+			$("body").css('marginLeft','14vw');
+
+		} else {
+			
+			$('#mySidenav').css('width','0');
+			$("body").css('marginLeft','0');			
+		
+		};
+		counter++;
+
+	});
+
+}
+
+})
